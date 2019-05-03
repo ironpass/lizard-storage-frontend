d25a880df7ee543a962b154ca66b3926cbe3de46
Akarachai Passavoranan (Ood) <akarachai45525@gmail.com>
2019-05-03 12:55:59 +0700

    first look on the app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './static/lizard.svg';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className='App'>
+//       <div className='Header'>
+//         <img id='lizard-logo' src={logo} alt='logo'/>
+//         <p id='project-name'>LIZARD STORAGE PROJECT</p>
+//       </div>
+//       <div className='FileList'>
+//         FileList
+//       </div>
+//     </div>
+//   )
+// }
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      files: [],
+
+    }
+  }
+  componentDidMount() {
+    axios.get('http://localhost:7777/showall').then((res) => {
+      console.log(res)
+      this.setState({files: res.data.map((file)=>file.filename)})
+    })
+    
+  }
+  generateList(fileList) {
+    return (
+      fileList.map((file)=>
+      <li>{file}</li>)
+    )
+  }
+  render() {
+    return(
+      <div className='App'>
+        <div className='Header'>
+          <img id='lizard-logo' src={logo} alt='logo' />
+          <p id='project-name'>LIZARD STORAGE PROJECT</p>  
+        </div>
+
+        <div className='Files'>
+          <ul id="filelist">
+            {this.generateList(this.state.files)}
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
-
 export default App;
