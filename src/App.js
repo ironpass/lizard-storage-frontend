f9eab9ad46f75573a55abc7e4b117b2b520a5a23
Akarachai Passavoranan (Ood) <akarachai45525@gmail.com>
2019-05-04 21:33:39 +0700

    finished download function

--- src/App.js
+++ src/App.js
@@ -27,13 +27,28 @@ class App extends React.Component {
     })   
   }
   
+  downloadFile(file) {
+    axios({
+      url: 'http://localhost:7777/download?id='+file.id,
+      method: 'GET',
+      responseType: 'blob', // important
+    }).then((res) => {
+      const url = window.URL.createObjectURL(new Blob([res.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', file.name);
+      document.body.appendChild(link);
+      link.click();
+    });
+  }
+
   generateList(fileList) {
     return (
         fileList.map((file)=>
         <div className='d-flex'>
           <ListGroupItem action className='fileitem'>
             {file.name}
-            <button className='downloadBtn' type='submit' onClick={()=>alert("nugget")}>
+            <button className='downloadBtn' type='submit' onClick={()=>this.downloadFile(file)}>
               <img className='downloadIcon' src={download} alt='download icon'/>
             </button>
             <button className='deleteBtn' type='submit' onClick={()=>(alert("nigga"))}>
